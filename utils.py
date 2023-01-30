ccf27222b5027d3bc71f2a8dafc3074e4ac159f5
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2023-01-29 20:31:57 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,6 +10,52 @@ def with_msgs(command):
     print(arcpy.GetMessages(0))
     print('-' * 100)
 
+    
+def rename_field(feature, current_field_name, new_field_name):
+
+    field_types = {
+        "String": "TEXT", "x": "FLOAT", "x": "DOUBLE", "Integer": "SHORT", "String": "LONG", "x": "DATE"
+    }
+
+    try:
+        field_props = [x for x in arcpy.ListFields(feature, current_field_name)][0]
+        print(f"\nArcpy Field Type: {field_props.type}")
+
+        field_type = field_types.get(field_props.type)
+
+        # Add new temp field
+        # Calculate temp field as existing field
+        temp_field_name = arcpy.ValidateFieldName(f"{new_field_name}_TEMP")
+
+        print(f"Creating temp field, '{temp_field_name}' from {current_field_name}...")
+        arcpy.CalculateField_management(
+            in_table=feature, field=temp_field_name, expression=f"!{current_field_name}!", expression_type="PYTHON3",
+            field_type=field_type
+        )
+
+        # Delete existing field
+        print(f"Deleting existing field, '{current_field_name}'...")
+        arcpy.DeleteField_management(in_table=feature, drop_field=current_field_name)
+
+        # Create new field with desired name
+        # Calculate new field with desired name
+        print(f"Creating new field, '{new_field_name}' from {temp_field_name}...")
+        arcpy.CalculateField_management(
+            in_table=feature, field=new_field_name, expression=f"!{temp_field_name}!", expression_type="PYTHON3",
+            field_type=field_type
+        )
+
+        print(f"Deleting temp field, '{temp_field_name}'...")
+        arcpy.DeleteField_management(in_table=feature, drop_field=temp_field_name)
+
+        return feature
+
+    except IndexError as e:
+        print(f"{current_field_name} does not exist in {feature}!")
+
+    except Exception as e:
+        print(e)
+
 
 def create_fgdb(out_folder_path, out_name="scratch.gdb"):
     """
